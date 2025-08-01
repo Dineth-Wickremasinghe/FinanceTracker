394258c7b131aed67cf7d1ed17e90fc70012094d
IT24100442 <it24100442@my.sliit.lk>
2025-08-01 06:21:16 +0530

    Initial commit

---
+++ src/main/java/org/example/financetracker/model/Entry.java
@@ -0,0 +1,13 @@
+package org.example.financetracker.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Entry {
+    private String name;
+    private String amount;
+    private String date;
+
+}
+
+
